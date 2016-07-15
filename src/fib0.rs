f5f37f3c019b9b3a2a4d66745d8aa94ba41d82fd
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2016-07-16 08:59:01 +0900

    Hello and Fibonacci

---
+++ src/fib0.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+fn main() {
+	let mut num = String::new();
+
+	io::stdin().read_line(&mut num)
+		.expect("Failed to read line");
+
+	let num: u64 = num.trim().parse()
+		.expect("Please type a number!");
+
+	println!("{}", fib(num).0);
+}
+
+fn fib(n: u64) -> (u64, u64) {
+	match n {
+		0 => (0, 1),
+		_ => {
+			let (a, b) = fib(n - 1);
+			(b, a + b)
+		}
+	}
+}
