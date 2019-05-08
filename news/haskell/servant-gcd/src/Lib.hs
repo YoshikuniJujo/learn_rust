1b9c3950d34d64d883d58a49680291e006bf11b8
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-05-08 10:46:01 +0900

    GCD server with Haskell Servant: done

--- news/haskell/servant-gcd/src/Lib.hs
+++ news/haskell/servant-gcd/src/Lib.hs
@@ -1,10 +1,12 @@
-{-# LANGUAGE DataKinds, TypeApplications, OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings, TypeApplications #-}
+{-# LANGUAGE DataKinds, TypeOperators #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Lib (startApp) where
 
-import Prelude hiding (head)
+import Prelude hiding (gcd)
 import Network.Wai.Handler.Warp
+import Web.FormUrlEncoded
 import Servant
 import Servant.HTML.Lucid
 import Lucid
@@ -22,7 +24,38 @@ instance ToHtml GetForm where
 			button_ [type_ "submit"] "Compute GCD"
 	toHtmlRaw = toHtml
 
-type API = Get '[HTML] GetForm
+data GcdArgs = GcdArgs { gcdArgN :: Word, gcdArgM :: Word } deriving Show
+
+instance FromForm GcdArgs where
+	fromForm form = do
+		ns <- parseAll "n" form
+		case ns of
+			[n, m] -> return $ GcdArgs n m
+			_ -> fail "Tow and only two parameters required"
+
+data ShowResult = ShowResult {
+	numN :: Word, numM :: Word, numGcd :: Word } deriving Show
+
+instance ToHtml ShowResult where
+	toHtml ShowResult { numN = n, numM = m, numGcd = g } = doctypehtml_ $ do
+		head_ . title_ $ toHtml ("GCD Calculator" :: T.Text)
+		body_ $ do
+			toHtml $ "The greatest common divisor of the numbers " ++
+				show n ++ " and " ++ show m ++ " is "
+			b_ . toHtml $ show g
+	toHtmlRaw = toHtml
+
+type API =
+	Get '[HTML] GetForm :<|>
+	"gcd" :> ReqBody '[FormUrlEncoded] GcdArgs :> Post '[HTML] ShowResult
 
 startApp :: IO ()
-startApp = run 8080 $ serve @API Proxy $ return GetForm
+startApp = run 8080 $ serve @API Proxy $
+	return GetForm :<|>
+	\GcdArgs { gcdArgN = n, gcdArgM = m } ->
+		return ShowResult { numN = n, numM = m, numGcd = gcd n m }
+
+gcd :: Word -> Word -> Word
+gcd n m | n > m = gcd m n
+gcd 0 m = m
+gcd n m = gcd (m `mod` n) n
