d16c0e04663edc188f33adcdf7ab2e5e0dc34fd3
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-05-08 09:06:11 +0900

    form with Haskell Servant

--- news/haskell/servant-gcd/src/Lib.hs
+++ news/haskell/servant-gcd/src/Lib.hs
@@ -1,23 +1,29 @@
-{-# LANGUAGE DataKinds, TypeApplications #-}
+{-# LANGUAGE DataKinds, TypeApplications, OverloadedStrings #-}
 {-# OPTIONS_GHC -Wall -fno-warn-tabs #-}
 
 module Lib (startApp) where
 
+import Text.Blaze.Html5 ((!))
 import Prelude hiding (head)
-
-import Text.Blaze.Html5
 import Network.Wai.Handler.Warp
 import Servant
 import Servant.HTML.Blaze
 
-data HelloWorld = HelloWorld deriving Show
+import qualified Data.Text as T
+import qualified Text.Blaze.Html5 as M
+import qualified Text.Blaze.Html5.Attributes as A
 
-instance ToMarkup HelloWorld where
-	toMarkup _ = docTypeHtml $ do
-		head . title $ toHtml "Say Hello"
-		body $ toHtml "Hello, world!"
+data GetForm = GetForm deriving Show
 
-type API = Get '[HTML] HelloWorld
+instance M.ToMarkup GetForm where
+	toMarkup _ = M.docTypeHtml $ do
+		M.head . M.title $ M.toHtml ("GCD Calculator" :: T.Text)
+		M.body . (M.form ! A.action "/gcd" ! A.method "post") $ do
+			M.input ! A.type_ "text" ! A.name "n"
+			M.input ! A.type_ "text" ! A.name "n"
+			M.button ! A.type_ "submit" $ "Compute GCD"
+
+type API = Get '[HTML] GetForm
 
 startApp :: IO ()
-startApp = run 8080 $ serve @API Proxy $ return HelloWorld
+startApp = run 8080 $ serve @API Proxy $ return GetForm
