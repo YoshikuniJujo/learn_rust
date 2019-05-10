dba0fc7e15c7f7cf13a6e90b9aa32de9b5647c83
Yoshikuni Jujo <PAF01143@nifty.ne.jp>
2019-05-10 15:34:25 +0900

    Mandelbrot with Haskell: Speed Chart: Result

---
+++ news/haskell/mandelbrot/mandel_speed.cmd
@@ -0,0 +1,1 @@
+time stack exec -- speed-chart mandel_speed.svg 5 1000x750 -1.20,0.35 -1,0.20 +RTS -N4
---
+++ news/haskell/mandelbrot/mandel_speed_mini.cmd
@@ -0,0 +1,1 @@
+time stack exec -- speed-chart mandel_speed_mini.svg 5 100x75 -1.20,0.35 -1,0.20 +RTS -N4
